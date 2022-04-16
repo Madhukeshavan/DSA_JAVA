2f4bbadbd19ae655545c64975d3e121ca932bbfd
Madhusudhan K <madhukeshavan2903@gmail.com>
2022-04-16 13:52:28 +0530

    added arrays sample

--- src/main/java/com/madhusudhan/TheArrays.java
+++ src/main/java/com/madhusudhan/TheArrays.java
@@ -1,7 +1,14 @@
 package com.madhusudhan;
 
+import java.util.Arrays;
+
 public class TheArrays {
     public static void main(String args[]){
-        System.out.println("Hello World");
+        String[] colors = new String[] {"red", "blue", "green"};
+        System.out.println(Arrays.toString(colors));
+
+        for(String a: colors){
+            System.out.println(a);
+        }
     }
 }
